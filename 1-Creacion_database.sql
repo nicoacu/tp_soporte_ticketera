c4d5c71df6772fc928c80cffc9069c9ad2643885
Nicolas Acuña <nicoacu87@gmail.com>
2024-11-18 11:15:43 -0300

    primer commit

---
+++ 1-Creacion_database.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE soporte_ticketera_2;
+
+USE soporte_ticketera_2;
+
+CREATE TABLE dbo.Clientes (
+    ClienteID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,  
+    Nombre VARCHAR(255) NOT NULL,                      
+    Direccion VARCHAR(255) NOT NULL,                   
+    Telefono VARCHAR(50) NOT NULL,                     
+    Email VARCHAR(255) NOT NULL,                       
+    FechaRegistro DATE NOT NULL                        
+);
+
+-- Crear tabla Tecnicos
+CREATE TABLE dbo.Tecnicos (
+    TecnicoID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,  
+    Nombre VARCHAR(255) NOT NULL,                     
+    Especialidad VARCHAR(100),                        
+    Disponibilidad BIT NOT NULL                       
+);
+
+-- Crear tabla Tickets
+CREATE TABLE dbo.Tickets (
+    TicketID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,   
+    FechaCreacion DATE NOT NULL,                      
+    ClienteID INT NOT NULL,                           
+    TecnicoID INT,                                    
+    Descripcion VARCHAR(MAX),
+    Estado VARCHAR(50) NOT NULL,                     
+    FOREIGN KEY (ClienteID) REFERENCES dbo.Clientes(ClienteID),
+    FOREIGN KEY (TecnicoID) REFERENCES dbo.Tecnicos(TecnicoID),
+    CONSTRAINT CK_Tickets_Estado CHECK (Estado IN ('Abierto', 'Cerrado', 'En progreso'))
+);
---
+++ 3-tecnicos_insert_statements.sql
@@ -0,0 +1,17 @@
+INSERT INTO soporte_ticketera_2.dbo.Tecnicos (Nombre, Especialidad, Disponibilidad)
+VALUES 
+('Laura Hernandez', 'IT Support Administrator', 0),
+('Amanda Garcia', 'IT Helpdesk Support', 1),
+('Jessica Martinez', 'IT Support Administrator', 0),
+('Sarah Brown', 'Technical Support Specialist', 0),
+('Christopher Thompson', 'Desktop Support Technician', 1),
+('Matthew Lopez', 'IT Support Supervisor', 1),
+('Laura Fernandez', 'Technical Support Specialist', 1),
+('Amanda Wilson', 'Desktop Support Technician', 0),
+('Laura Davis', 'IT Support Specialist', 0),
+('Christopher Thompson', 'IT Support Supervisor', 0),
+('Michael Davis', 'Service Desk Analyst', 0),
+('Melissa Thompson', 'IT Support Supervisor', 0),
+('Jessica Christenson', 'IT Support Coordinator', 0),
+('David Wilson', 'IT Support Team Lead', 0),
+('David Thompson', 'Help Desk Technician', 1);
